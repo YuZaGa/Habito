cecc7802eee1ffec9448e66b79313b70e6b56957
Yumn Gauhar <yumangr@gmail.com>
2023-03-30 19:47:13 +0530

    Updated Home UI

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -37,7 +37,7 @@ _appBar() {
   return Container(
     padding: const EdgeInsets.only(top: 10, left: 20, right: 15),
     child: Row(
-      mainAxisAlignment: MainAxisAlignment.end,
+      //mainAxisAlignment: MainAxisAlignment.end,
       children: [
         Expanded(
           child: Column(
@@ -77,13 +77,13 @@ String getGreeting() {
   var hour = now.hour;
 
   if (hour >= 5 && hour < 12) {
-    return 'morning';
+    return 'Good Morning';
   } else if (hour >= 12 && hour < 17) {
-    return 'afternoon';
+    return 'Good Afternoon';
   } else if (hour >= 17 && hour < 21) {
-    return 'evening';
+    return 'Good Evening';
   } else {
-    return 'hey';
+    return 'Hey';
   }
 }
 
@@ -137,58 +137,149 @@ _showTask() {
       valueListenable: Hive.box<Task>('tasks').listenable(),
       builder: (context, Box<Task> box, _) {
         List<Task> tasks = box.values.toList();
-        return ListView.builder(
-          itemCount: tasks.length,
-          itemBuilder: (context, index) {
-            Task task = tasks[index];
-            return Container(
-              height: 150,
-              decoration: BoxDecoration(
-                color: Color(0xFFDDEDEC),
-                borderRadius: BorderRadius.circular(12),
-                boxShadow: [
-                  BoxShadow(
-                    color: Color(0xFFFBEBCC),
-                    spreadRadius: 1,
-                    blurRadius: 5,
-                    offset: Offset(0, 3),
-                  ),
-                ],
-              ),
-              margin: const EdgeInsets.only(
-                  top: 15, left: 18, right: 18, bottom: 0),
-              padding: const EdgeInsets.only(right: 10),
-              child: Slidable(
-                endActionPane: ActionPane(
-                  motion: const StretchMotion(),
-                  children: [
-                    // settings option
-                    SlidableAction(
-                      onPressed: null,
-                      backgroundColor: Colors.grey.shade800,
-                      icon: Icons.edit,
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    SizedBox(width: 5),
-
-                    // delete option
-                    SlidableAction(
-                      onPressed: (BuildContext context) {
-                        box.delete(task.key);
-                      },
-                      backgroundColor: Colors.red.shade400,
-                      icon: Icons.delete,
-                      borderRadius: BorderRadius.circular(12),
+        return Container(
+          padding: EdgeInsets.only(bottom: 30),
+          child: ListView.builder(
+            itemCount: tasks.length,
+            itemBuilder: (context, index) {
+              Task task = tasks[index];
+              return Container(
+                height: 130,
+                decoration: BoxDecoration(
+                  color: Color(0xFFDDEDEC),
+                  borderRadius: BorderRadius.circular(12),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Color(0xFFFBEBCC),
+                      spreadRadius: 1,
+                      blurRadius: 5,
+                      offset: Offset(0, 3),
                     ),
                   ],
                 ),
-                child: ListTile(
-                  title: Text(task.title),
-                  subtitle: Text(task.note),
+                margin: const EdgeInsets.only(
+                    top: 15, left: 18, right: 18, bottom: 0),
+                padding: const EdgeInsets.only(right: 10),
+                child: Slidable(
+                  endActionPane: ActionPane(
+                    motion: const StretchMotion(),
+                    children: [
+                      // settings option
+                      SlidableAction(
+                        onPressed: null,
+                        backgroundColor: Colors.grey.shade800,
+                        icon: Icons.edit,
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      SizedBox(width: 5),
+
+                      // delete option
+                      SlidableAction(
+                        onPressed: (BuildContext context) {
+                          box.delete(task.key);
+                        },
+                        backgroundColor: Colors.red.shade400,
+                        icon: Icons.delete,
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ],
+                  ),
+                  child: ListTile(
+                    title: Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            task.title.toLowerCase(),
+                            style: GoogleFonts.lato(
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                            ),
+                          ),
+                          SizedBox(height: 2),
+                          Text(
+                            task.note,
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 2,
+                            style: GoogleFonts.lato(
+                                fontSize: 15,
+                                fontWeight: FontWeight.w600,
+                                color: Colors.grey),
+                          ),
+                          SizedBox(height: 15),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                children: [
+                                  Column(
+                                    children: [
+                                      Text(
+                                        "duration",
+                                        style: GoogleFonts.lato(
+                                            fontSize: 10,
+                                            fontWeight: FontWeight.w500,
+                                            color: Colors.grey),
+                                      ),
+                                      Row(
+                                        children: [
+                                          Text(
+                                            task.startTime,
+                                            style: GoogleFonts.lato(
+                                                fontSize: 15,
+                                                fontWeight: FontWeight.w600,
+                                                color: Colors.black),
+                                          ),
+                                          Text(
+                                            " - " + task.endTime,
+                                            style: GoogleFonts.lato(
+                                                fontSize: 15,
+                                                fontWeight: FontWeight.w600,
+                                                color: Colors.black),
+                                          ),
+                                        ],
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              ),
+                              Column(
+                                children: [
+                                  Column(
+                                    children: [
+                                      Text(
+                                        "streaks",
+                                        style: GoogleFonts.lato(
+                                            fontSize: 10,
+                                            fontWeight: FontWeight.w500,
+                                            color: Colors.grey),
+                                      ),
+                                      Row(
+                                        children: [
+                                          Text(
+                                            "10 days",
+                                            style: GoogleFonts.lato(
+                                                fontSize: 15,
+                                                fontWeight: FontWeight.w600,
+                                                color: Colors.black),
+                                          ),
+                                        ],
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
                 ),
-              ),
-            );
-          },
+              );
+            },
+          ),
         );
       },
     ),
